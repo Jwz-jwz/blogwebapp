4003d067e8ade38ccba88e6c2d84a2e83fdc3f22
Jwz-jwz <jnglobal444@gmail.com>
2024-08-28 23:20:14 +0800

    hero navbar nemev

--- src/components/svg/Hero.jsx
+++ src/components/svg/Hero.jsx
@@ -8,8 +8,18 @@ export const Hero = () => {
         <div className="relative">
           <img className="container" src="./mainpic.png" alt="" />
 
-          <div className="w-[200px] h-[200px] bg-red-200 absolute left-[10px] bottom-[10px]">
-            hello
+          <div className="flex flex-col gap-[24px] absolute left-[10px] bottom-[10px] bg-[var(--textwhite)] p-[40px] rounded-[12px] border-[1px] border-[var(--bordercolor)]">
+            <div className="flex flex-col gap-[16px]">
+              <p className="py-[4px] px-[10px] rounded-[6px] bg-[#4B6BFB] text-[14px] leading-[20px] font-[500] text-[var(--textwhite)]">
+                Technology
+              </p>
+              <p className="text-[36px] leading-[40px] font-[600]">
+                Grid system for better Design User Interface
+              </p>
+            </div>
+            <p className="text-[16px] leading-[24px] font-[400] text-[var(--textgrey)]">
+              August 20, 2022
+            </p>
           </div>
         </div>
         <div className="flex justify-end gap-[8px] ">
