a8da8cddbbbbaad349437e8a4f4a6e0a06263024
Jwz-jwz <jnglobal444@gmail.com>
2024-08-28 19:25:13 +0800

    position ynzlav hero

--- src/components/svg/Hero.jsx
+++ src/components/svg/Hero.jsx
@@ -4,8 +4,14 @@ import { RightSumIcon } from "./RightSumIcon";
 export const Hero = () => {
   return (
     <div className="w-full relative top-[100px] flex justify-center">
-      <div className="container flex flex-col gap-[8px]">
-        <img className="container" src="./mainpic.png" alt="" />
+      <div className=" container flex flex-col gap-[8px]">
+        <div className="relative">
+          <img className="container" src="./mainpic.png" alt="" />
+
+          <div className="w-[200px] h-[200px] bg-red-200 absolute left-[10px] bottom-[10px]">
+            hello
+          </div>
+        </div>
         <div className="flex justify-end gap-[8px] ">
           <button>
             <LeftSumIcon />
